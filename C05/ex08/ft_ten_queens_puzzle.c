8e6333129fdbd833d68994aed0f3f82ac8d45e95
kreis <kreis@student.42malaga.com>
2025-07-09 21:57:32 +0200

    g

--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,35 +6,40 @@
 /*   By: kreys <kreys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:43:12 by kreys             #+#    #+#             */
-/*   Updated: 2025/07/08 19:56:51 by kreys            ###   ########.fr       */
+/*   Updated: 2025/07/09 21:46:22 by kreys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void	fill_arr(char arr[10][10], int i, int b)
+#define FSIZE 10
+
+void	fill_arr(char arr[FSIZE][FSIZE])
 {
+	int	i;
+	int	b;
+
 	i = -1;
-	while (++i < 10)
+	while (++i < FSIZE)
 	{
 		b = -1;
-		while (++b < 10)
+		while (++b < FSIZE)
 			arr[i][b] = '-';
 	}
 }
 
-int	print_arr(char arr[10][10], int *c_w)
+int	print_comb(char arr[FSIZE][FSIZE], int *c_w)
 {
 	int	i;
 	int	d;
 	int	to_print;
 
 	i = -1;
-	while (++i < 10)
+	while (++i < FSIZE)
 	{
 		d = -1;
-		while (++d < 10)
+		while (++d < FSIZE)
 		{
 			if (arr[d][i] == 'Q')
 			{
@@ -48,15 +53,15 @@ int	print_arr(char arr[10][10], int *c_w)
 	return (-1);
 }
 
-int	fill_field(char arr[10][10], int row, int coll)
+int	put_queen(char arr[FSIZE][FSIZE], int row, int coll)
 {
 	int	i;
 	int	d;
 
 	i = 0;
-	if (row > 9 || coll > 9)
+	if (row > FSIZE - 1 || coll > FSIZE - 1)
 		return (-1);
-	while (i < 10)
+	while (i < FSIZE)
 	{
 		if (arr[row][i] == 'Q')
 			return (-1);
@@ -65,32 +70,32 @@ int	fill_field(char arr[10][10], int row, int coll)
 	}
 	i = row;
 	d = coll;
-	while (i < 10 && i >= 0 && d < 10 && d >= 0)
+	while (i < FSIZE && i >= 0 && d < FSIZE && d >= 0)
 		if (arr[i--][d--] == 'Q')
 			return (-1);
 	i = row;
 	d = coll;
-	while (i < 10 && i >= 0 && d < 10 && d >= 0)
+	while (i < FSIZE && i >= 0 && d < FSIZE && d >= 0)
 		if (arr[i++][d--] == 'Q')
 			return (-1);
 	arr[row][coll] = 'Q';
 	return (0);
 }
 
-int	build_all(int start_i, int start_b, char arr[10][10], int *count_win)
+int	build_all(int start_i, int start_b, char arr[FSIZE][FSIZE], int *count_win)
 {
 	int		start_ii;
 	int		start_bb;
 	int		cc;
 
-	cc = fill_field(arr, start_i, start_b++);
+	cc = put_queen(arr, start_i, start_b++);
 	if (cc == -1)
 		return (-1);
 	start_ii = start_i;
 	start_bb = start_b - 1;
-	if (start_b == 10)
+	if (start_b == FSIZE)
 	{
-		cc = print_arr(arr, count_win);
+		cc = print_comb(arr, count_win);
 		arr[start_ii][start_bb] = '-';
 		return (cc);
 	}
@@ -98,7 +103,7 @@ int	build_all(int start_i, int start_b, char arr[10][10], int *count_win)
 	while (cc != 1 && start_ii > ++start_i)
 		cc = build_all(start_i, start_b, arr, count_win);
 	start_i = start_ii;
-	while (cc != 1 && ++start_i <= 9)
+	while (cc != 1 && ++start_i <= FSIZE - 1)
 		cc = build_all(start_i, start_b, arr, count_win);
 	if (cc == -1)
 		arr[start_ii][start_bb] = '-';
@@ -110,13 +115,13 @@ int	ft_ten_queens_puzzle(void)
 	int		start_i;
 	int		start_b;
 	int		count_win;
-	char	arr[10][10];
+	char	arr[FSIZE][FSIZE];
 
 	count_win = 0;
 	start_b = 0;
 	start_i = -1;
-	fill_arr(arr, 0, 0);
-	while (++start_i < 10)
+	fill_arr(arr);
+	while (++start_i < FSIZE)
 		build_all(start_i, start_b, arr, &count_win);
 	return (count_win);
 }
